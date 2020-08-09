f9513671dc4c5ad508a3ebabe8701899d7f57aa1
masterUNG <phrombutr@gmail.com>
2020-08-09 16:01:15 +0700

    Finish 9Aug

--- lib/widget/page0.dart
+++ lib/widget/page0.dart
@@ -10,8 +10,26 @@ class _Page0State extends State<Page0> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Page 0'),
+        title: Text('ขายสินค้า'),
+      ),
+      body: Column(
+        children: [
+          buildAddButton(),
+          Text('body'),
+          Text('body'),
+          Text('body'),
+          Text('body'),
+          Text('body'),
+          Text('body'),
+        ],
       ),
     );
   }
+
+  Widget buildAddButton() => Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(icon: Icon(Icons.add_circle), onPressed: null),
+          IconButton(icon: Icon(Icons.remove_circle), onPressed: null),
+        ],
+      );
 }
